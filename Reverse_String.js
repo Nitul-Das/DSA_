0f661795f0397a7e2f4b1d3d193d95d2971415e6
Nitul Das <nituldas8753@gmail.com>
2025-02-14 01:08:11 +0530

    ds

---
+++ Reverse_String.js
@@ -0,0 +1,12 @@
+let reverseString = function(s){
+   let i = 0,j = s.length-1;
+    while(i<j){
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;   
+    }
+}
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
---
+++ Sorted_Array.js
@@ -0,0 +1,13 @@
+var isSorted = function(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            return false; 
+        }
+    }
+    return true; 
+};
+
+console.log(isSorted([1, 2, 3, 4])); // true
+
+console.log(isSorted([1, 3, 2, 4])); // false
+

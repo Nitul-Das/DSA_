ab2f6ac557b082d5e8f62d52be21d9fda78db533
Nitul Das <nituldas8753@gmail.com>
2025-02-16 23:53:04 +0530

    added files

--- valid_Palindrome.js
+++ valid_Palindrome.js
@@ -1,3 +1,28 @@
-var isPalindrome = function(s) {
+var validPalindrome = function(s) {
+    let i = 0, j = s.length - 1; 
+
+    while (i < j) { 
+        if (!/[a-zA-Z0-9]/.test(s[i])) {
+            i++; 
+            continue;
+        }
+        
+        if (!/[a-zA-Z0-9]/.test(s[j])) {
+            j--; 
+            continue;
+        }
+
+        if (s[i].toLowerCase() !== s[j].toLowerCase()) {
+            return false; 
+        }
+
+        i++;
+        j--;
+    }
     
+    return true; 
 };
+
+console.log(validPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(validPalindrome("race a car")); // false
+console.log(validPalindrome(" ")); //
